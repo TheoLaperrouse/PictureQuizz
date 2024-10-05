f6d508e901f61890426367cdf743bdeac620cea2
Théo Laperrouse <theo.laperrouse@energiency.com>
2024-10-05 09:50:07 +0200

    fix: press enter to send answer

--- src/components/AnswerInput.vue
+++ src/components/AnswerInput.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="flex space-x-2">
+    <div class="flex w-full">
         <input
             v-model="answer"
             type="text"
             placeholder="Entrez votre réponse"
-            class="p-2 rounded-lg border border-gray-300 text-black w-64"
+            class="p-2 rounded-lg border border-gray-300 text-black flex-grow mr-2"
+            @keydown.enter="submit"
         />
         <button
             @click="submit"
-            class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300"
+            class="bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-lg min-w-[200px] transition duration-300"
         >
             {{ $t('submitAnswer') }}
         </button>
